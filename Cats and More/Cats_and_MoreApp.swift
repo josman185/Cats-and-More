4f63662402dc4cd546e40430f318223c24994436
josman185 <josman185@hotmail.com>
2024-06-03 16:28:34 -0600

    Initial Commit

---
+++ Cats and More/Cats_and_MoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_and_MoreApp.swift
+//  Cats and More
+//
+//  Created by Jose Vargas on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Cats_and_MoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats and MoreUITests/Cats_and_MoreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cats_and_MoreUITestsLaunchTests.swift
+//  Cats and MoreUITests
+//
+//  Created by Jose Vargas on 6/3/24.
+//
+
+import XCTest
+
+final class Cats_and_MoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
