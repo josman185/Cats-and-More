6db447331fda2c3127bd49904efc05aeef7324b8
josman185 <josman185@hotmail.com>
2024-06-04 18:49:11 -0600

    adds downloadThumb func

--- Cats and More/Managers/NetworkManager.swift
+++ Cats and More/Managers/NetworkManager.swift
@@ -48,4 +48,31 @@ final class NetworkManager {
         }
         task.resume()
     }
+    
+    func downloadThumbnail(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, _ in
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        task.resume()
+    }
 }
